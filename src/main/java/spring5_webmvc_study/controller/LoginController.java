4adeb440a9cd0fe54d775ac0bc6919dd7f0f44c3
yjkim <chitose_@naver.com>
2021-05-25 11:14:39 +0900

    이메일 기억하기(쿠키에저장) 서블렛(디스패처) 오류발생 main 안켜지뮤

--- src/main/java/spring5_webmvc_study/controller/LoginController.java
+++ src/main/java/spring5_webmvc_study/controller/LoginController.java
@@ -1,10 +1,13 @@
 package spring5_webmvc_study.controller;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,20 +20,32 @@ public class LoginController {
 	private AuthService authService;
 
 	@GetMapping
-	public String form(LoginCommand loginCommand) {
+	public String form(LoginCommand loginCommand,@CookieValue(value="REMEMBER",required=false) Cookie rCookie) {
+		if(rCookie !=null) {
+			loginCommand.setEmail(rCookie.getValue());  //멤버테이블 쓰고있는 loginCommand(dto)에Email세팅(Cookie에 있는 값?이름?)
+			loginCommand.setRememberEmail(true);
+		}
 		return "/login/loginForm";
 
 	}
 
 	@PostMapping
-	public String submit(LoginCommand loginCommand, Errors errors, HttpSession session) {
+	public String submit(LoginCommand loginCommand, Errors errors, HttpSession session, HttpServletResponse response) {
 		new LoginCommandValidator().validate(loginCommand, errors);
 		if (errors.hasErrors())
 			return "/login/loginForm";
 		try {
 			AuthInfo authInfo = authService.authenicate(loginCommand.getEmail(), loginCommand.getPassword());
-			// 세션에 authInfo 저장해야한당
-			session.setAttribute("authInfo", authInfo);
+			session.setAttribute("authInfo", authInfo);		// 세션에 authInfo 저장해야한당
+			
+			Cookie rememberCookie = new Cookie("REMEMBER",loginCommand.getEmail());
+			rememberCookie.setPath("/");
+			if(loginCommand.isRememberEmail()) {
+				rememberCookie.setMaxAge(60*60*24*30);
+			}else {
+				rememberCookie.setMaxAge(0);
+			}response.addCookie(rememberCookie);
+			
 			return "/login/loginSuccess";
 		} catch (WrongIdPasswordException ex) {
 			errors.reject("idPasswordNotMatching");
